6d8f43b74e24e48523fa6cf200a375176dfe16ec
swademead <swademead@gmail.com>
2018-03-04 17:12:10 -0500

    added chapter 2

--- timeseriesanalysis.r
+++ timeseriesanalysis.r
@@ -83,3 +83,106 @@ ts.plot(eu_stocks, col = 1:4, xlab = "Year", ylab = "Index Value", main = "Major
 
 # Add a legend to your ts.plot
 legend("topleft", colnames(eu_stocks), lty = 1, col = 1:4, bty = "n")
+
+#CHAPTER 2
+# Log rapid_growth
+linear_growth <- log(rapid_growth)
+  
+# Plot linear_growth using ts.plot()
+ts.plot(linear_growth) 
+
+# Generate the first difference of z
+dz <- diff(z)
+  
+# Plot dz
+ts.plot(dz)
+
+# View the length of z and dz, respectively
+length(z)
+length(dz)
+
+# Generate a diff of x with lag = 4. Save this to dx
+dx <- diff(x, lag = 4)
+  
+# Plot dx
+  ts.plot(dx)
+
+# View the length of x and dx, respectively 
+length(x)
+length(dx)
+
+# Simulate a WN model with list(order = c(0, 0, 0))
+white_noise <- arima.sim(model = list(c(0, 0, 0)), n = 100)
+
+# Plot your white_noise data
+ts.plot(white_noise)
+
+# Simulate from the WN model with: mean = 100, sd = 10
+white_noise_2 <- arima.sim(model = list(c(0, 0, 0)), n = 100, mean = 100, sd = 10)
+
+# Plot your white_noise_2 data
+ts.plot(white_noise_2)
+
+# Fit the WN model to y using the arima command
+arima(y, order = c(0, 0, 0))
+
+# Calculate the sample mean and sample variance of y
+mean(y)
+var(y)
+
+# Generate a RW model using arima.sim
+random_walk <- arima.sim(model = list(order = c(0, 1, 0)), n = 100)
+
+# Plot random_walk
+ts.plot(random_walk)
+
+# Calculate the first difference series
+random_walk_diff <- diff(random_walk)
+
+# Plot random_walk_diff
+ts.plot(random_walk_diff)
+
+# Generate a RW model with a drift uing arima.sim
+rw_drift <- arima.sim(model = list(order = c(0, 1, 0)), n = 100, mean = 1)
+
+# Plot rw_drift
+ts.plot(rw_drift)
+
+# Calculate the first difference series
+rw_drift_diff <- diff(rw_drift)
+
+# Plot rw_drift_diff
+ts.plot(rw_drift_diff)
+
+# Difference your random_walk data
+rw_diff <- diff(random_walk)
+
+# Plot rw_diff
+ts.plot(rw_diff)
+
+# Now fit the WN model to the differenced data
+model_wn <- arima(rw_diff, order = c(0, 0, 0))
+
+# Store the value of the estimated time trend (intercept)
+int_wn <- model_wn$coef
+
+# Plot the original random_walk data
+ts.plot(random_walk)
+
+# Use abline(0, ...) to add time trend to the figure
+abline(0, int_wn)
+
+# Use arima.sim() to generate WN data
+white_noise <- arima.sim(model = list(order = c(0, 0, 0)), n = 100)
+
+# Use cumsum() to convert your WN data to RW
+random_walk <- cumsum(white_noise)
+  
+# Use arima.sim() to generate WN drift data
+wn_drift <- arima.sim(model = list(order = c(0, 0, 0)), n = 100, mean = 0.4)
+  
+# Use cumsum() to convert your WN drift data to RW
+rw_drift <- cumsum(wn_drift)
+
+# Plot all four data objects
+plot.ts(cbind(white_noise, random_walk, wn_drift, rw_drift))
