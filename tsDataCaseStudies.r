5b5a8d0f6bd4fc92c9ca2fc6f7ce29035261df92
swademead <swademead@gmail.com>
2018-03-06 13:28:47 -0500

    added chapter 2

--- tsDataCaseStudies.r
+++ tsDataCaseStudies.r
@@ -71,3 +71,89 @@ class(flights_xts2)
 
 # Examine the first five rows of your new flights_xts2 object
 head(flights_xts2)
+
+#CHAPTER 2
+# View the structure of each object
+str(temps_1)
+str(temps_2)
+
+# View the first and last rows of temps_1
+head(temps_1)
+tail(temps_1)
+
+# View the first and last rows of temps_2
+head(temps_2)
+tail(temps_2)
+
+# Confirm that the date column in each object is a time-based class
+class(temps_1$date)
+class(temps_2$date)
+
+# Encode your two temperature data frames as xts objects
+temps_1_xts <- as.xts(temps_1[, -4], order.by = temps_1$date)
+temps_2_xts <- as.xts(temps_2[, -4], order.by = temps_2$date)
+
+# View the first few lines of each new xts object to confirm they are properly formatted
+head(temps_1_xts)
+head(temps_2_xts)
+
+# Use rbind to merge your new xts objects
+temps_xts <- rbind(temps_1_xts, temps_2_xts)
+
+# View data for the first 3 days of the last month of the first year in temps_xts
+first(last(first(temps_xts, "1 year"), "1 month"), "3 days")
+
+# Identify the periodicity of temps_xts
+periodicity(temps_xts)
+
+# Generate a plot of mean Boston temperature for the duration of your data
+plot.xts(temps_xts$mean)
+
+# Generate a plot of mean Boston temperature from November 2010 through April 2011
+plot.xts(temps_xts$mean["2010-11/2011-04"])
+
+# Use plot.zoo to generate a single plot showing mean, max, and min temperatures during the same period 
+plot.zoo(temps_xts["2010-11/2011-04"], plot.type = "single", lty = lty)
+
+# Subset your temperature data to include only 2010 through 2015: temps_xts_2
+temps_xts_2 <- temps_xts["2010/2015"]
+
+# Use to.period to convert temps_xts_2 to monthly periodicity
+temps_monthly <- to.period(temps_xts_2, period = "months", OHLC = FALSE, indexAt = "firstof")
+
+# Compare the periodicity and duration of temps_monthly and flights_xts 
+periodicity(temps_monthly)
+periodicity(flights_xts)
+
+# Split temps_xts_2 into separate lists per month
+monthly_split <- split(temps_xts_2$mean , f = "months")
+
+# Use lapply to generate the monthly mean of mean temperatures
+mean_of_means <- lapply(monthly_split, FUN = mean)
+
+# Use as.xts to generate an xts object of average monthly temperature data
+temps_monthly <- as.xts(as.numeric(mean_of_means), order.by = index)
+ 
+# Compare the periodicity and duration of your new temps_monthly and flights_xts 
+periodicity(temps_monthly)
+periodicity(flights_xts)
+
+# Use merge to combine your flights and temperature objects
+flights_temps <- merge(flights_xts, temps_monthly)
+
+# Examine the first few rows of your combined xts object
+head(flights_temps)
+
+# Use plot.zoo to plot these two columns in a single panel
+plot.zoo(flights_temps[,c("pct_delay", "temps_monthly")], plot.type = "single", lty = lty)
+legend("topright", lty = lty, legend = labels, bg = "white")
+
+# Confirm the periodicity and duration of the vis and wind data
+periodicity(vis)
+periodicity(wind)
+
+# Merge vis and wind with your existing flights_temps data
+flights_weather <- merge(flights_temps, vis, wind) 
+
+# View the first few rows of your flights_weather data
+head(flights_weather)
