5ed07899e905af787ca07aec4edc3f9e787d44a7
swademead <swademead@gmail.com>
2018-03-08 16:36:09 -0500

    added chapter 3

--- visualization.r
+++ visualization.r
@@ -162,3 +162,49 @@ acf(rtn)
 
 qqnorm(rtn)
 qqline(rtn, col = "red")
+
+#CHAPTER 3
+# Plot stacked barplot
+barplot(portfolio)
+
+# Plot grouped barplot
+barplot(portfolio, beside = TRUE)
+
+# Draw the scatterplot
+plot(sp500, citi)
+
+# Draw a regression line
+abline(reg = lm(citi ~ sp500), col = "red", lwd = 2)
+
+# Create correlation matrix using Pearson method
+cor(my_data)
+
+# Create correlation matrix using Spearman method
+cor(my_data, method = "spearman")
+
+# Create scatterplot matrix
+pairs(my_data)
+
+# Create upper panel scatterplot matrix
+pairs(my_data, lower.panel = NULL)
+
+# Create correlation matrix
+corrplot(cor_mat)
+
+# Create correlation matrix with numbers
+corrplot(cor_mat, method = "number" )
+
+# Create correlation matrix with colors
+corrplot(cor_mat, method = "color")
+
+# Create upper triangle correlation matrix
+corrplot(cor_mat, method = "number", type = "upper")
+
+# Draw heatmap of cor_mat
+corrplot(cor_mat, method = "color")
+
+# Draw upper heatmap
+corrplot(cor_mat, method = "color", type = "upper")
+
+# Draw the upper heatmap with hclust
+corrplot(cor_mat, method = "color", type = "upper", order = "hclust")
