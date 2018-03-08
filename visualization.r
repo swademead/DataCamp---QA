32e46e216619a8937347c489a5b28ef0fe55851b
swademead <swademead@gmail.com>
2018-03-08 17:39:22 -0500

    added chapter 4

--- visualization.r
+++ visualization.r
@@ -208,3 +208,57 @@ corrplot(cor_mat, method = "color", type = "upper")
 
 # Draw the upper heatmap with hclust
 corrplot(cor_mat, method = "color", type = "upper", order = "hclust")
+
+#CHAPTER 4
+# Plot the portfolio value
+plot(data$value, main = "Portfolio Value")
+
+# Plot the portfolio return
+plot(data$return, main = "Portfolio Return")
+
+# Plot a histogram of portfolio return 
+hist(data$return, probability = TRUE)
+
+# Add a density line
+lines(density(data$return), col = "red", lwd = 2)
+
+# Plot the four stocks on the same graphical window
+par(mfrow = c(2, 2), mex = 0.8, cex = 0.8)
+plot(data$GS)
+plot(data$KO)
+plot(data$DIS)
+plot(data$CAT)
+
+# Draw the scatterplot of gs against the portfolio
+plot(gs, portfolio)
+
+# Add a regression line in red
+abline(reg = lm(gs ~ portfolio), col = "red", lwd = 2)
+
+# Plot scatterplots and regression lines to a 2x2 window
+par(mfrow = c(2, 2))
+plot(gs, portfolio)
+abline(reg = lm(gs ~ portfolio), col = "red", lwd = 2)
+plot(ko, portfolio)
+abline(reg = lm(ko ~ portfolio), col = "red", lwd = 2)
+plot(dis, portfolio)
+abline(reg = lm(dis ~ portfolio), col = "red", lwd = 2)
+plot(cat, portfolio)
+abline(reg = lm(cat ~ portfolio), col = "red", lwd = 2)
+
+# Plot new and old portfolio values on same chart
+plot(old.vs.new.portfolio$old.portfolio.value)
+lines(old.vs.new.portfolio$new.portfolio.value, col = "red")
+
+# Plot density of the new and old portfolio returns on same chart
+plot(density(old.vs.new.portfolio$old.portfolio.rtn))
+lines(density(old.vs.new.portfolio$new.portfolio.rtn), col = "red")
+
+# Draw value, return, drawdowns of old portfolio
+charts.PerformanceSummary(old.vs.new.portfolio$old.portfolio.rtn)
+
+# Draw value, return, drawdowns of new portfolio
+charts.PerformanceSummary(old.vs.new.portfolio$new.portfolio.rtn)
+
+# Draw both portfolios on same chart
+charts.PerformanceSummary(old.vs.new.portfolio[,3:4])
