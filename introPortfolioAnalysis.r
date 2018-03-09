c0819059c30f9690a6927a5b027ffdfe87f92344
swademead <swademead@gmail.com>
2018-03-09 18:49:54 -0500

    added chapter 3

--- introPortfolioAnalysis.r
+++ introPortfolioAnalysis.r
@@ -205,3 +205,84 @@ table.Drawdowns(sp500_monthly)
 
 # Plot of drawdowns
 chart.Drawdown(sp500_monthly)
+
+#CHAPTER 3
+# Create a grid
+grid <- seq(from = 0, to = 1, by = 0.01)
+
+# Initialize an empty vector for sharpe ratios
+vsharpe <- rep(NA, times = length(grid) )
+
+# Create a for loop to calculate Sharpe ratios
+for(i in 1:length(grid)) {
+	weight <- grid[i]
+	preturns <- weight * returns_equities + (1 - weight) * returns_bonds
+	vsharpe[i] <- SharpeRatio.annualized(preturns)
+}
+
+# Plot weights and Sharpe ratio
+plot(grid, vsharpe, xlab = "Weights", ylab= "Ann. Sharpe ratio")
+abline(v = grid[vsharpe == max(vsharpe)], lty = 3)
+
+# Create a scatter plot
+chart.Scatter(returns_bonds, returns_equities)
+
+# Find the correlation
+cor(returns_equities, returns_bonds)
+
+# Merge returns_equities and returns_bonds 
+returns <- merge(returns_equities, returns_bonds)
+
+# Find and visualize the correlation using chart.Correlation
+chart.Correlation(returns)
+
+# Visualize the rolling estimates using chart.RollingCorrelation
+chart.RollingCorrelation(returns_equities, returns_bonds, width = 24)
+
+# Create a vector of returns 
+means <- apply(returns, 2, "mean")
+  
+# Create a vector of standard deviation
+sds <- apply(returns, 2, "sd")
+
+# Create a scatter plot
+plot(sds, means)
+text(sds, means, labels = colnames(returns), cex = 0.7)
+abline(h = 0, lty = 3)
+
+# Create a matrix with variances on the diagonal
+diag_cov <- diag(sds^2)
+
+# Create a covariance matrix of returns
+cov_matrix <- cov(returns)
+
+# Create a correlation matrix of returns
+cor_matrix <- cor(returns)
+
+# Verify covariances equal the product of standard deviations and correlation
+all.equal(cov_matrix[1,2], cor_matrix[1,2] * sds[1] * sds[2])
+
+# Create a weight matrix w
+w <- as.matrix(weights)
+
+# Create a matrix of returns
+mu <- as.matrix(vmeans)
+
+# Calculate portfolio mean monthly returns
+t(w) %*% mu
+
+# Calculate portfolio volatility
+sqrt(t(w) %*% sigma %*% w)
+
+# Create portfolio weights
+weights <- c(0.4, 0.4, 0.1, 0.1)
+
+# Create volatility budget
+vol_budget <- StdDev(returns, portfolio_method = "component", weights = weights)
+
+# Make a table of weights and risk contribution
+weights_percrisk <- cbind(weights, vol_budget$pct_contrib_StdDev)
+colnames(weights_percrisk) <- c("weights", "perc vol contrib")
+
+# Print the table
+weights_percrisk
