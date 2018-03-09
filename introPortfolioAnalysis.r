0281a2f86ac9b63a5a856934d39cab271261910e
swademead <swademead@gmail.com>
2018-03-09 16:49:15 -0500

    added chapter 2

--- introPortfolioAnalysis.r
+++ introPortfolioAnalysis.r
@@ -93,3 +93,115 @@ eop_weight_rebal <- pf_rebal$EOP.Weight
 par(mfrow = c(2, 1), mar=c(2, 4, 2, 2))
 plot.zoo(eop_weight_bh$AAPL)
 plot.zoo(eop_weight_rebal$AAPL)
+
+#CHAPTER 2
+# Convert the daily frequency of sp500 to monthly frequency: sp500_monthly
+sp500_monthly <- to.monthly(sp500)
+
+# Print the first six rows of sp500_monthly
+head(sp500_monthly, n = 6)
+
+# Create sp500_returns using Return.calculate using the closing prices
+sp500_returns <- Return.calculate(sp500_monthly$sp500.Close)
+
+# Time series plot
+plot.zoo(sp500_returns)
+
+# Produce the year x month table
+table.CalendarReturns(sp500_returns)
+
+# Compute the mean monthly returns
+mean(sp500_returns)
+
+# Compute the geometric mean of monthly returns
+mean.geometric(sp500_returns)
+
+# Compute the standard deviation
+sd(sp500_returns)
+
+# Compute the annualized risk free rate
+annualized_rf <- (1 + rf)^12 - 1
+
+# Plot the annualized risk free rate
+plot.zoo(annualized_rf)
+
+# Compute the series of excess portfolio returns 
+sp500_excess <- sp500_returns - rf
+
+# Compare the mean
+mean(sp500_returns)
+mean(sp500_excess)
+
+# Compute the Sharpe ratio
+sp500_sharpe <- mean(sp500_excess) / sd(sp500_returns)
+
+# Compute the annualized mean
+Return.annualized(sp500_returns)
+
+# Compute the annualized standard deviation
+StdDev.annualized(sp500_returns)
+
+# Compute the annualized Sharpe ratio: ann_sharpe
+ann_sharpe <- SharpeRatio.annualized(sp500_returns)
+
+# Compute all of the above at once using table.AnnualizedReturns()
+table.AnnualizedReturns(sp500_returns)
+
+# Calculate the mean, volatility, and sharpe ratio of sp500_returns
+returns_ann <- Return.annualized(sp500_returns)
+sd_ann <- StdDev.annualized(sp500_returns)
+sharpe_ann <- SharpeRatio.annualized(sp500_returns, rf)
+
+# Plotting the 12-month rolling annualized mean
+chart.RollingPerformance(R = sp500_returns, width = 12, FUN = "Return.annualized")
+abline(h = returns_ann)
+
+# Plotting the 12-month rolling annualized standard deviation
+chart.RollingPerformance(R = sp500_returns, width = 12, FUN = "StdDev.annualized")
+abline(h = sd_ann)
+
+# Plotting the 12-month rolling annualized Sharpe ratio
+chart.RollingPerformance(R = sp500_returns, width = 12, FUN = "SharpeRatio.annualized", Rf= rf)
+abline(h = sharpe_ann)
+
+# Fill in window for 2008
+sp500_2008 <- window(sp500_returns, start = "2008-01-01", end = "2008-12-31")
+
+# Create window for 2014
+sp500_2014 <- window(sp500_returns, start = "2014-01-01", end = "2014-12-31")
+
+# Plotting settings
+par(mfrow = c(1, 2) , mar=c(3, 2, 2, 2))
+names(sp500_2008) <- "sp500_2008"
+names(sp500_2014) <- "sp500_2014"
+
+# Plot histogram of 2008
+chart.Histogram(sp500_2008, methods = c("add.density", "add.normal"))
+
+# Plot histogram of 2014
+chart.Histogram(sp500_2014, methods = c("add.density", "add.normal"))
+
+#  Compute the skewness 
+  skewness(sp500_daily)
+  skewness(sp500_monthly)
+
+# Compute the excess kurtois 
+kurtosis(sp500_daily)
+kurtosis(sp500_monthly)
+   
+   # Calculate the SemiDeviation
+SemiDeviation(sp500_monthly)
+
+# Calculate the value at risk
+VaR(sp500_monthly, p = 0.025)
+VaR(sp500_monthly, p = 0.05)
+
+# Calculate the expected shortfall
+ES(sp500_monthly, p = 0.025)
+ES(sp500_monthly, p = 0.05)
+
+# Table of drawdowns
+table.Drawdowns(sp500_monthly)
+
+# Plot of drawdowns
+chart.Drawdown(sp500_monthly)
