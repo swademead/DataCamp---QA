76a57ff105ccbf038e16d80a3613c0c22dc05add
swademead <swademead@gmail.com>
2018-03-12 17:21:24 -0400

    added chapter 2

--- riskManagement.r
+++ riskManagement.r
@@ -138,3 +138,139 @@ plot(maturity, first(zcb), ylim = range(zcb), type = "l", ylab = "yield (%)", co
 
 # Add a line for the last day of zcb
 lines(maturity, last(zcb))
+
+#CHAPTER 2
+# Calculate average and standard deviation of djx
+mu <- mean(djx)
+sigma <- sd(djx)
+
+# Plot histogram of djx
+hist(djx, nclass = 20, probability = TRUE)
+
+# Add the normal density as a red line to histogram
+lines(djx, dnorm(djx, mu, sigma), col = "red")
+
+# Plot non-parametric KDE of djx
+plot(density(djx))
+
+# Add the normal density as red line to KDE
+lines(djx, dnorm(djx, mu, sigma), col = "red")
+
+# Make a Q-Q plot of djx and add a red line
+qqnorm(djx)
+qqline(djx, col = "red")
+
+# Calculate the length of djx as n
+n = length(djx)
+
+# Generate n standard normal variables, make a Q-Q plot, add a red line
+x1 <- rnorm(n)
+qqnorm(x1)
+qqline(x1, col = "red")
+
+# Generate n Student t variables, make a Q-Q plot, add a red line
+x2 <- rt(n, df = 4)
+qqnorm(x2)
+qqline(x2, col = "red")
+
+# Generate n standard uniform variables, make a Q-Q plot, add red line
+x3 <- runif(n)
+qqnorm(x3)
+qqline(x3, col = "red")
+
+# Calculate skewness and kurtosis of djx
+skewness(djx)
+kurtosis(djx)
+
+# Carry out a Jarque-Bera test for djx
+jarque.test(djx)
+
+# Calculate skewness and kurtosis of djreturns 
+s <- apply(djreturns, 2, skewness)
+k <- apply(djreturns, 2, kurtosis)
+
+# Plot k against s and add text labels to identify stocks
+plot(s, k, type = "n")
+text(s, k, names(s), cex = 0.6)
+
+# Carry out Jarque-Bera tests for each constituent in djreturns
+apply(djreturns, 2, jarque.test)
+
+# Calculate weekly and monthly log-returns from djx_d
+djx_w <- apply.weekly(djx_d,colSums)
+djx_m <- apply.monthly(djx_d, colSums)
+
+# Calculate the p-value for each series in djx_d
+apply(djx_d, 2, function(v){jarque.test(v)$p.value})
+
+# Calculate the p-value for each series in djx_w
+apply(djx_w, 2, function(v){jarque.test(v)$p.value})
+
+# Calculate the p-value for each series in djx_m
+apply(djx_m, 2, function(v){jarque.test(v)$p.value})
+
+# Calculate a 21-day moving sum of djx
+djx21 <- rollapplyr(djx, width = 21, FUN = sum)[-c(1:20)]
+
+# Calculate a 63-day moving sum of djx
+djx63 <- rollapplyr(djx, width = 63, FUN = sum)[-c(1:62)]
+
+# Merge the three series and plot
+djx2 <- merge(djx, djx21, djx63, all = FALSE)
+plot.zoo(djx2)
+
+# Compute the skewness and kurtosis for each series in djx2
+apply(djx2, 2, skewness)
+apply(djx2, 2, kurtosis)
+
+# Conduct the Jarque-Bera test to each series in djx2
+apply(djx2, 2, jarque.test)
+
+# Fit a Student t distribution to djx
+tfit <- fit.st(djx)
+
+# Define tpars, nu, mu, and sigma
+tpars <- tfit$par.ests
+nu <- tpars["nu"]
+mu <- tpars["mu"]
+sigma <- tpars["sigma"]
+
+# Plot a histogram of djx
+hist(djx, nclass = 20, probability = TRUE, ylim = range(0, 40))
+
+# Compute the fitted t density at the values djx
+yvals <- dt((djx - mu)/sigma, df = nu)/sigma
+
+# Superimpose a red line to show the fitted t density
+lines(djx, yvals, col = "red")
+
+# Plot the daily log-return series in fx_d
+plot.zoo(fx_d)
+
+# Apply the Jarque-Bera test to each of the series in fx_d
+apply(fx_d, 2, jarque.test)
+
+# Plot the monthly log-return series in fx_m
+plot.zoo(fx_m, type = "h")
+
+# Apply the Jarque-Bera test to each of the series in fx_m
+apply(fx_m, 2, jarque.test)
+
+# Fit a Student t distribution to each of the series in fx_m
+apply(fx_m, 2, function(v){fit.st(v)$par.ests})
+
+# Plot the interest-rate return series zcbx_m and zcbx2_m
+plot.zoo(zcbx_m, type = "h")
+plot.zoo(zcbx2_m, type = "h")
+
+# Make Q-Q plots of the 3rd component series of zcbx_m and zcbx2_m
+qqnorm(zcbx_m[,3])
+qqnorm(zcbx2_m[, 3])
+
+# Compute the kurtosis of each series in zcbx_m and zcbx2_m
+apply(zcbx_m, 2, kurtosis)
+apply(zcbx2_m, 2, kurtosis)
+
+# Conduct the Jarque-Bera test on each series in zcbx_m and zcbx2_m
+apply(zcbx_m, 2, jarque.test)
+apply(zcbx2_m, 2, jarque.test)
