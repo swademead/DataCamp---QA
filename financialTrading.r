b4b7995bd79c5b2495ddc4d3f82af52ec0c17129
swademead <swademead@gmail.com>
2018-03-14 13:34:15 -0400

    added chapter 2

--- financialTrading.r
+++ financialTrading.r
@@ -14,3 +14,51 @@ plot(Cl(SPY))
 
 # Add a 200-day SMA using lines()
 lines(SMA(Cl(SPY), n = 200), col = "red")
+
+#CHAPTER 2
+# Load the quantstrat package
+library(quantstrat)
+
+# Create initdate, from, and to strings
+initdate <- "1999-01-01"
+from <- "2003-01-01"
+to <- "2015-12-31"
+
+# Set the timezone to UTC
+Sys.setenv(TZ = "UTC")
+
+# Set the currency to USD 
+currency("USD")
+
+# Load the quantmod package
+library(quantmod)
+
+# Retrieve SPY from yahoo
+getSymbols("SPY", src = "yahoo", from = from, to = to, adjust = TRUE)
+
+# Use stock() to initialize SPY and set currency to USD
+stock("SPY", currency = "USD")
+
+# Define your trade size and initial equity
+tradesize <- 100000
+initeq <- 100000
+
+# Define the names of your strategy, portfolio and account
+strategy.st <- "firststrat"
+portfolio.st <- "firststrat"
+account.st <- "firststrat"
+
+# Remove the existing strategy if it exists
+rm.strat(strategy.st)
+
+# Initialize the portfolio
+initPortf(portfolio.st, symbols = "SPY", initDate = initdate, currency = "USD")
+
+# Initialize the account
+initAcct(account.st, portfolios = portfolio.st, initDate = initdate, currency = "USD", initEq = initeq)
+
+# Initialize the orders
+initOrders(portfolio.st, initDate = initdate)
+
+# Store the strategy
+strategy(strategy.st, store = TRUE)
