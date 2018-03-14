15897e64c4fd3d38d1e8821b521b8eada61929ea
swademead <swademead@gmail.com>
2018-03-14 11:04:52 -0400

    added chapter 1

---
+++ financialTrading.r
@@ -0,0 +1,16 @@
+#CHAPTER 1
+# Get SPY from yahoo
+getSymbols("SPY", 
+           from = "2000-01-01", 
+           to = "2016-06-30", 
+           src =  "yahoo", 
+           adjust =  TRUE)
+
+# Plot the closing price of SPY
+plot(Cl(SPY))
+
+# Plot the closing prices of SPY
+plot(Cl(SPY))
+
+# Add a 200-day SMA using lines()
+lines(SMA(Cl(SPY), n = 200), col = "red")
